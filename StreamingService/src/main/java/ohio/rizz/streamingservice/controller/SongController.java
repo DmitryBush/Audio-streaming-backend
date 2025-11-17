0cd3ea50ed3d71cb743914082516f546f7bdea53
Gregory Kochetkov <chloromail@gmail.com>
2025-11-17 23:01:19 +0300

    Удалена тестовая html страница. Удалена зависимость Thymeleaf. Рефакторинг: создан контроллер для песен SongController, AudioController переименован в UploadController, изменены адреса во всех контроллерах.

---
+++ StreamingService/src/main/java/ohio/rizz/streamingservice/controller/SongController.java
@@ -0,0 +1,55 @@
+package ohio.rizz.streamingservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import ohio.rizz.streamingservice.dto.song.SongReadDto;
+import ohio.rizz.streamingservice.service.song.SongService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.NoSuchElementException;
+
+@Controller
+@RequestMapping("/api/v1/songs")
+@RequiredArgsConstructor
+public class SongController {
+
+    private final SongService songService;
+    private final PagedResourcesAssembler<SongReadDto> assembler;
+
+    @GetMapping("/song/{songId}")
+    @ResponseBody
+    public ResponseEntity<SongReadDto> getSong(@PathVariable Long songId) {
+        try {
+            SongReadDto song = songService.findById(songId);
+            return ResponseEntity.ok(song);
+        } catch (NoSuchElementException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @GetMapping("")
+    @ResponseBody
+    public ResponseEntity<PagedModel<EntityModel<SongReadDto>>> listSongs(@PageableDefault(size = 15) Pageable pageable) {
+        try {
+            Page<SongReadDto> songs = songService.findAllSongs(pageable);
+            return ResponseEntity.ok(assembler.toModel(songs));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+}
--- StreamingService/src/main/java/ohio/rizz/streamingservice/controller/StreamingController.java
+++ StreamingService/src/main/java/ohio/rizz/streamingservice/controller/StreamingController.java
@@ -1,39 +1,24 @@
 package ohio.rizz.streamingservice.controller;
 
 import lombok.RequiredArgsConstructor;
-import ohio.rizz.streamingservice.Entities.AudioMetadata;
-import ohio.rizz.streamingservice.dto.song.SongReadDto;
 import ohio.rizz.streamingservice.service.song.AudioMetadataService;
 import ohio.rizz.streamingservice.service.song.SongService;
 import ohio.rizz.streamingservice.service.storage.ObjectStorageService;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.support.ResourceRegion;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.PagedModel;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.core.io.Resource;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Controller
-@RequestMapping("/api/v1/audio")
+@RequestMapping("/api/v1/streaming")
 @RequiredArgsConstructor
 public class StreamingController {
     private final SongService songService;
     private final AudioMetadataService metadataService;
     private final ObjectStorageService objectStorageService;
-    private final PagedResourcesAssembler<SongReadDto> assembler;
 
     private final int maxChunkSizeBytes = 1024 * 1024;
 
@@ -76,37 +61,6 @@ public class StreamingController {
                         .contentLength(rangeLength)
                         .body(resource);
             }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }
-
-    @GetMapping("/song/{songId}")
-    @ResponseBody
-    public ResponseEntity<SongReadDto> getSong(@PathVariable Long songId) {
-        try {
-            SongReadDto song = songService.findById(songId);
-            return ResponseEntity.ok(song);
-        } catch (NoSuchElementException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-    }
-
-    @GetMapping("/songs-list")
-    public String songsPage(Model model, Pageable pageable) {
-        Page<SongReadDto> songs = songService.findAllSongs(pageable);
-        model.addAttribute("songs", songs);
-        return "songs";
-    }
-
-    @GetMapping("/songs")
-    @ResponseBody
-    public ResponseEntity<PagedModel<EntityModel<SongReadDto>>> listSongs(@PageableDefault(size = 15) Pageable pageable) {
-        try {
-            Page<SongReadDto> songs = songService.findAllSongs(pageable);
-            return ResponseEntity.ok(assembler.toModel(songs));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
