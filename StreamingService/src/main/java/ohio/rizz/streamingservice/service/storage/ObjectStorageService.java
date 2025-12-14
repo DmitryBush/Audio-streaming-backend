ae1e93e893b048c150b6ab5a03c948de8fb235e4
Dmitry Bush <bushuev0458@gmail.com>
2025-12-15 00:18:35 +0300

    performance(storage):Implemented upload with input stream

--- StreamingService/src/main/java/ohio/rizz/streamingservice/service/storage/ObjectStorageService.java
+++ StreamingService/src/main/java/ohio/rizz/streamingservice/service/storage/ObjectStorageService.java
@@ -2,20 +2,15 @@ package ohio.rizz.streamingservice.service.storage;
 
 import io.minio.*;
 import io.minio.errors.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Service;
+import ohio.rizz.streamingservice.service.storage.exception.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.core.io.*;
+import org.springframework.scheduling.annotation.*;
+import org.springframework.stereotype.*;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.concurrent.CompletableFuture;
+import java.io.*;
+import java.security.*;
+import java.util.concurrent.*;
 
 @Service
 public class ObjectStorageService {
@@ -31,22 +26,34 @@ public class ObjectStorageService {
                 .build();
     }
 
-    public void saveFile(File file, String bucket, String object) {
+    public void saveFile(InputStream inputStream, Long streamSize, String bucket, String objectReference) {
         try {
             boolean isBucketExist = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucket).build());
             if (!isBucketExist) {
                 minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucket).build());
             }
 
-            minioClient.uploadObject(UploadObjectArgs.builder()
-                                             .bucket(bucket)
-                                             .object(object)
-                                             .filename(file.getAbsolutePath())
-                                             .build());
+            minioClient.putObject(PutObjectArgs.builder()
+                    .bucket(bucket)
+                    .object(objectReference)
+                    .stream(inputStream, streamSize, -1)
+                    .build());
         } catch (ErrorResponseException | InsufficientDataException | InternalException | InvalidKeyException |
                  InvalidResponseException | IOException | NoSuchAlgorithmException | ServerException |
                  XmlParserException e) {
-            throw new RuntimeException(e);
+            throw new ObjectStorageException(e.getMessage(), e.getCause());
+        }
+    }
+
+    @Async("fileUploadTaskExecutor")
+    public CompletableFuture<Void> saveFileAsync(InputStream inputStream, long streamSize, String bucket, String objectReference) {
+        CompletableFuture<Void> future = new CompletableFuture<>();
+        try {
+            saveFile(inputStream, streamSize, bucket, objectReference);
+            future.complete(null);
+            return future;
+        } catch (RuntimeException e) {
+            throw new ObjectStorageException(e.getMessage(), e.getCause());
         }
     }
 
@@ -60,6 +67,25 @@ public class ObjectStorageService {
             future.completeExceptionally(e);
         }
         return future;
+    }
+
+    public void saveFile(File file, String bucket, String object) {
+        try {
+            boolean isBucketExist = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucket).build());
+            if (!isBucketExist) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucket).build());
+            }
+
+            minioClient.uploadObject(UploadObjectArgs.builder()
+                    .bucket(bucket)
+                    .object(object)
+                    .filename(file.getAbsolutePath())
+                    .build());
+        } catch (ErrorResponseException | InsufficientDataException | InternalException | InvalidKeyException |
+                 InvalidResponseException | IOException | NoSuchAlgorithmException | ServerException |
+                 XmlParserException e) {
+            throw new ObjectStorageException(e.getMessage(), e.getCause());
+        }
     }
 
     public void deleteFile(String bucket, String object) {
@@ -76,8 +102,8 @@ public class ObjectStorageService {
     }
 
     public Resource loadResource(String bucket, String object) {
-        try(InputStream inputStream = minioClient.getObject(GetObjectArgs.builder()
-                                                                    .bucket(bucket).object(object).build())) {
+        try (InputStream inputStream = minioClient.getObject(GetObjectArgs.builder()
+                .bucket(bucket).object(object).build())) {
             Resource resource = new ByteArrayResource(inputStream.readAllBytes());
             if (resource.exists() && resource.isReadable()) {
                 return resource;
@@ -93,8 +119,8 @@ public class ObjectStorageService {
     public InputStreamResource loadStreamResource(String bucket, String object, Long startPosition, Long chunkSize) {
         try {
             InputStream inputStream = minioClient.getObject(GetObjectArgs.builder()
-                                                                    .bucket(bucket).object(object).offset(startPosition)
-                                                                    .length(chunkSize).build());
+                    .bucket(bucket).object(object).offset(startPosition)
+                    .length(chunkSize).build());
             return new InputStreamResource(inputStream);
         } catch (ServerException | InternalException | XmlParserException | InsufficientDataException |
                  ErrorResponseException | IOException | NoSuchAlgorithmException | InvalidKeyException |
