28e157154ba0632a35c2920fefe8796bb810c780
Dmitry Bush <bushuev0458@gmail.com>
2025-12-17 15:25:37 +0300

    Implemented UserDetailsService in UserService

--- UserService/src/main/java/com/bush/user/service/UserService.java
+++ UserService/src/main/java/com/bush/user/service/UserService.java
@@ -8,16 +8,22 @@ import com.bush.user.repository.UserRepository;
 import com.bush.user.service.mapper.UserCreateMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class UserService {
+public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
 
@@ -61,5 +67,13 @@ public class UserService {
                 }, () -> {
                     throw new ResponseStatusException(HttpStatus.NOT_FOUND);
                 });
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username)
+                .map(user -> new User(user.getLogin(), user.getPassword(), List.of(
+                        new SimpleGrantedAuthority("ROLE_" + user.getRole().getRoleName().name()))))
+                .orElseThrow(() -> new UsernameNotFoundException("Error in username or password"));
     }
 }
