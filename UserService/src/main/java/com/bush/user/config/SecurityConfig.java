44b0a1f60fb307721a9af53789f4a857f019b2b0
Dmitry Bush <bushuev0458@gmail.com>
2025-12-17 17:01:30 +0300

    Implemented argon2 password encryption

--- UserService/src/main/java/com/bush/user/config/SecurityConfig.java
+++ UserService/src/main/java/com/bush/user/config/SecurityConfig.java
@@ -1,18 +1,35 @@
 package com.bush.user.config;
 
 import com.bush.user.entity.RoleEnum;
+import com.bush.user.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    // Configuration parameters for Argon2
+    @Value("${spring.security.encryption-config.salt-length}")
+    private Integer saltLength;
+    @Value("${spring.security.encryption-config.hash-length}")
+    private Integer hashLength;
+    @Value("${spring.security.encryption-config.parallelism}")
+    private Integer parallelism;
+    @Value("${spring.security.encryption-config.memory}")
+    private Integer memory;
+    @Value("${spring.security.encryption-config.iterations}")
+    private Integer iterations;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
@@ -37,5 +54,13 @@ public class SecurityConfig {
                         .logoutSuccessUrl("/")
                         .deleteCookies("JSESSIONID"))
                 .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserService userService) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userService);
+        authenticationProvider.setPasswordEncoder(
+                new Argon2PasswordEncoder(saltLength, hashLength, parallelism, memory, iterations));
+        return authenticationProvider;
     }
 }
