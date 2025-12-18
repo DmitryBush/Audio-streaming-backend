f10e4315464bec5c5e0c4f2a7197ccebd5fa7e12
Dmitry Bush <bushuev0458@gmail.com>
2025-12-18 14:53:40 +0300

    Implemented TypeConverterService

---
+++ UserService/src/main/java/com/bush/user/service/type/converter/TypeConverterService.java
@@ -0,0 +1,25 @@
+package com.bush.user.service.type.converter;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class TypeConverterService {
+    public <T> List<T> convertObjectToList(Object object, Class<T> clazz) {
+        if (Objects.isNull(object)) {
+            return Collections.emptyList();
+        }
+
+        if (object.getClass().isAssignableFrom(Collection.class)) {
+            Collection<?> collection = (Collection<?>) object;
+            return collection.stream()
+                    .map(clazz::cast)
+                    .toList();
+        }
+        throw new ClassCastException("Object parameter isn't a Collection class");
+    }
+}
