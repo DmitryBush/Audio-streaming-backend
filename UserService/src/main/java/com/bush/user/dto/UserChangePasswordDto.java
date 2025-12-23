27f3d227c876ab635649216c71be2d029d356cdf
Dmitry Bush <bushuev0458@gmail.com>
2025-12-23 17:34:50 +0300

    fix(security):fixed incorrect credential validating

--- UserService/src/main/java/com/bush/user/dto/UserChangePasswordDto.java
+++ UserService/src/main/java/com/bush/user/dto/UserChangePasswordDto.java
@@ -1,10 +1,9 @@
 package com.bush.user.dto;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
 
-public record UserChangePasswordDto(@NotBlank @Min(4) @Max(32) String login,
-                                    @NotBlank @Min(8) @Max(255) String oldPassword,
-                                    @NotBlank @Min(8) @Max(255) String newPassword) {
+public record UserChangePasswordDto(@NotBlank @Length(min = 4, max = 32) String login,
+                                    @NotBlank @Length(min = 8, max = 255) String oldPassword,
+                                    @NotBlank @Length(min = 8, max = 255) String newPassword) {
 }
--- UserService/src/main/java/com/bush/user/dto/UserLoginDto.java
+++ UserService/src/main/java/com/bush/user/dto/UserLoginDto.java
@@ -1,9 +1,8 @@
 package com.bush.user.dto;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
 
-public record UserLoginDto(@NotBlank @Min(4) @Max(32) String login,
-                           @NotBlank @Min(8) @Max(255) String password) {
+public record UserLoginDto(@NotBlank @Length(min = 4, max = 32) String login,
+                           @NotBlank @Length(min = 8, max = 255) String password) {
 }
