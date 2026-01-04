5de1b10207cef0bba6deaea07bbf3418039736d3
Dmitry Bush <bushuev0458@gmail.com>
2026-01-04 23:19:45 +0300

    feat:Implemented controller initialization

---
+++ SearchService/src/main/java/com/bush/search/service/connector/DebeziumConnectorInitializerService.java
@@ -0,0 +1,43 @@
+package com.bush.search.service.connector;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DebeziumConnectorInitializerService {
+    private final DebeziumConnectorService debeziumConnectorService;
+
+    @Value("${spring.debezium.request-body-path}")
+    private String requestBodyPath;
+
+    @EventListener(ApplicationReadyEvent.class)
+    protected void initializeConnectorsAfterBoot() throws IOException {
+        ResourcePatternResolver patternResolver = new PathMatchingResourcePatternResolver();
+        Resource[] jsonRequestResources = patternResolver.getResources(requestBodyPath);
+        for (Resource resource : jsonRequestResources) {
+            File file = resource.getFile();
+            if (resource.getFile().getName().endsWith(".json")) {
+                createConnector(file.getName().substring(0, file.getName().indexOf('.')), resource);
+            }
+        }
+    }
+
+    private void createConnector(String connectorName, Resource jsonResource) throws IOException {
+        if (!debeziumConnectorService.isConnectorExist(connectorName)) {
+            debeziumConnectorService.registerConnector(jsonResource.getInputStream());
+            log.info("Registered connector {}", connectorName);
+        }
+    }
+}
---
+++ SearchService/src/main/java/com/bush/search/service/connector/DebeziumConnectorService.java
@@ -0,0 +1,68 @@
+package com.bush.search.service.connector;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DebeziumConnectorService {
+    @Value("${spring.debezium.url}")
+    private String debeziumUrl;
+
+    public boolean isConnectorExist(String connectorName) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            URI url = UriComponentsBuilder.fromUriString(debeziumUrl)
+                    .path("/")
+                    .pathSegment("connectors")
+                    .pathSegment(connectorName)
+                    .build()
+                    .toUri();
+
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
+            return response.getStatusCode().is2xxSuccessful();
+        } catch (HttpClientErrorException e) {
+            return false;
+        }
+    }
+
+    public void registerConnector(InputStream inputStream) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            URI url = UriComponentsBuilder.fromUriString(debeziumUrl)
+                    .path("/")
+                    .pathSegment("connectors")
+                    .build()
+                    .toUri();
+
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST,
+                    new HttpEntity<>(inputStream.readAllBytes()), String.class);
+            if (!response.getStatusCode().is2xxSuccessful()) {
+                throw new HttpClientErrorException(response.getStatusCode());
+            }
+        } catch (HttpClientErrorException e) {
+            log.error("An error has occurred with status {}", e.getStatusCode());
+            throw e;
+        } catch (RestClientException e) {
+            log.error("An error has occurred while generating request");
+            throw e;
+        } catch (IOException e) {
+            log.error("An error has occurred while reading json");
+            throw new RuntimeException(e.getCause());
+        }
+    }
+}
