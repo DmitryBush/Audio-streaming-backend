65c1b726bde2707f30ed23780520f716e4db5de4
Dmitry Bush <bushuev0458@gmail.com>
2026-01-07 13:23:38 +0300

    feat:Injecting secrets system  updated

--- SearchService/src/main/java/com/bush/search/service/connector/DebeziumConnectorInitializerService.java
+++ SearchService/src/main/java/com/bush/search/service/connector/DebeziumConnectorInitializerService.java
@@ -1,5 +1,7 @@
 package com.bush.search.service.connector;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,17 +14,32 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class DebeziumConnectorInitializerService {
     private final DebeziumConnectorService debeziumConnectorService;
+    private final ObjectMapper objectMapper;
 
     @Value("${spring.debezium.request-body-path}")
     private String requestBodyPath;
+
+    @Value("${spring.datasource.metadata.name}")
+    private String metadataDatabaseName;
+    @Value("${spring.datasource.metadata.username}")
+    private String metadataDatabaseUsername;
+    @Value("${spring.datasource.metadata.password}")
+    private String metadataDatabasePassword;
+
+    @Value("${spring.datasource.playlist.name}")
+    private String playlistDatabaseName;
+    @Value("${spring.datasource.playlist.username}")
+    private String playlistDatabaseUsername;
+    @Value("${spring.datasource.playlist.password}")
+    private String playlistDatabasePassword;
 
     @EventListener(ApplicationReadyEvent.class)
     protected void initializeConnectorsAfterBoot() throws IOException {
@@ -38,8 +55,27 @@ public class DebeziumConnectorInitializerService {
 
     private void createConnector(String connectorName, Resource jsonResource) throws IOException {
         if (!debeziumConnectorService.isConnectorExist(connectorName)) {
-            debeziumConnectorService.registerConnector(jsonResource.getContentAsString(StandardCharsets.UTF_8));
+            debeziumConnectorService.registerConnector(getJsonStringConnectorConfiguration(jsonResource));
             log.info("Registered connector {}", connectorName);
         }
+    }
+
+    private String getJsonStringConnectorConfiguration(Resource jsonResource) throws IOException {
+        String resourceContentString = jsonResource.getContentAsString(StandardCharsets.UTF_8);
+        Map<String, Object> jsonConnectorConfigObjectMap = objectMapper.readValue(resourceContentString,
+                new TypeReference<>() {});
+
+        Map<String, String> connectorConfigurationMap = objectMapper.convertValue(jsonConnectorConfigObjectMap.get("config"),
+                new TypeReference<>() {});
+        String databaseName = connectorConfigurationMap.get("database.dbname");
+        if (metadataDatabaseName.equals(databaseName)) {
+            resourceContentString = resourceContentString.replace("$POSTGRES_USERNAME", metadataDatabaseUsername);
+            return resourceContentString.replace("$POSTGRES_PASSWORD", metadataDatabasePassword);
+        } else if (playlistDatabaseName.equals(databaseName)) {
+            resourceContentString = resourceContentString.replace("$POSTGRES_USERNAME", playlistDatabaseUsername);
+            return resourceContentString.replace("$POSTGRES_PASSWORD", playlistDatabasePassword);
+        }
+        log.error("Unknown database name: {}", databaseName);
+        throw new IllegalArgumentException("Unknown database name");
     }
 }
