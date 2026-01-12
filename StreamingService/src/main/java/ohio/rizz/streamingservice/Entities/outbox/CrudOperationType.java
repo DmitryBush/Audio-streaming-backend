bc57621d02453ec3723165739ee78540f450852f
Dmitry Bush <bushuev0458@gmail.com>
2026-01-12 15:07:59 +0300

    refactor:removed unused code

--- StreamingService/src/main/java/ohio/rizz/streamingservice/Entities/outbox/CrudOperationType.java
+++
@@ -1,5 +0,0 @@
-package ohio.rizz.streamingservice.Entities.outbox;
-
-public enum CrudOperationType {
-    C, R, U, D
-}
--- StreamingService/src/main/java/ohio/rizz/streamingservice/Repositories/OutboxMetadataRepository.java
+++
@@ -1,9 +0,0 @@
-package ohio.rizz.streamingservice.Repositories;
-
-import ohio.rizz.streamingservice.Entities.outbox.OutboxMetadata;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface OutboxMetadataRepository extends JpaRepository<OutboxMetadata, UUID> {
-}
