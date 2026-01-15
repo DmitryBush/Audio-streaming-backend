918a11f8f8eaf2a8f7ced33b619926b0f076bf03
Dmitry Bush <bushuev0458@gmail.com>
2026-01-15 12:37:22 +0300

    feat:Changed event structure for accord to outbox

--- SearchService/src/main/java/com/bush/search/domain/index/ChangeDataEvent.java
+++ SearchService/src/main/java/com/bush/search/domain/index/ChangeDataEvent.java
@@ -1,6 +1,15 @@
 package com.bush.search.domain.index;
 
-import com.bush.search.domain.index.service.scheme.Schema;
+import com.bush.outbox.domain.entity.CrudOperationType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 
-public record ChangeDataEvent<T>(Schema schema, T payload) {
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+public record ChangeDataEvent<T>(@JsonProperty("operation_id") UUID operationId,
+                                 @JsonProperty("object_name") String objectName,
+                                 @JsonProperty("operation_type") CrudOperationType operationType,
+                                 @JsonProperty("created_at") ZonedDateTime createdAt,
+                                 @JsonUnwrapped T payload) {
 }
