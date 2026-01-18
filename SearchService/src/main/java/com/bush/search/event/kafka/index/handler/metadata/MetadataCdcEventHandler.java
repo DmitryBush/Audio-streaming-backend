56f5d1c3da2ed434e212be1ec6a011d0f01d1c2a
Dmitry Bush <bushuev0458@gmail.com>
2026-01-18 15:59:57 +0300

    feat(event handler):added different group id

--- SearchService/src/main/java/com/bush/search/event/kafka/index/handler/metadata/MetadataCdcEventHandler.java
+++ SearchService/src/main/java/com/bush/search/event/kafka/index/handler/metadata/MetadataCdcEventHandler.java
@@ -6,17 +6,19 @@ import com.bush.search.event.kafka.index.handler.strategy.ResolveIndexStrategyRe
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
-@KafkaListener(topics = "search-metadata.public.outbox_service_table")
+@KafkaListener(topics = "search-metadata.public.outbox_service_table", groupId = "metadata-processing-group",
+        containerFactory = "concurrentKafkaListenerContainerFactory")
 @RequiredArgsConstructor
 public class MetadataCdcEventHandler implements CdcEventHandler<String> {
     private final ResolveIndexStrategyRegistry resolveIndexStrategyRegistry;
 
     @KafkaHandler
     @Override
-    public void handle(ChangeDataEvent<String> changeDataEvent) {
+    public void handle(@Payload ChangeDataEvent<String> changeDataEvent) {
         resolveIndexStrategyRegistry.resolveIndexStrategy(changeDataEvent.payload(), changeDataEvent.operationType(),
                 changeDataEvent.objectName());
     }
--- SearchService/src/main/java/com/bush/search/event/kafka/index/handler/playlist/PlaylistCdcEventHandler.java
+++ SearchService/src/main/java/com/bush/search/event/kafka/index/handler/playlist/PlaylistCdcEventHandler.java
@@ -6,19 +6,23 @@ import com.bush.search.event.kafka.index.handler.CdcEventHandler;
 import com.bush.search.event.kafka.index.handler.strategy.crud.CrudOperationStrategyRegistry;
 import com.bush.search.service.playlist.PlaylistService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
-@KafkaListener(topics = "search-playlist.public.outbox_service_table")
+@KafkaListener(topics = "search-playlist.public.outbox_service_table", groupId = "playlist-processing-group",
+        containerFactory = "concurrentKafkaListenerContainerFactory")
 @RequiredArgsConstructor
 public class PlaylistCdcEventHandler implements CdcEventHandler<PlaylistPayload> {
     private final CrudOperationStrategyRegistry crudOperationStrategyRegistry;
 
     @KafkaHandler
     @Override
-    public void handle(ChangeDataEvent<PlaylistPayload> changeDataEvent) {
+    public void handle(@Payload ChangeDataEvent<PlaylistPayload> changeDataEvent) {
         crudOperationStrategyRegistry.processStrategy(changeDataEvent.payload(), changeDataEvent.operationType(),
                 PlaylistService.class);
     }
