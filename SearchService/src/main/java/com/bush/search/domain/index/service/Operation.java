c0da54549a24bbf9cadcdf0facd9b02bf6deed73
Dmitry Bush <bushuev0458@gmail.com>
2026-01-18 15:56:57 +0300

    fix(json):fixed crud Operation codes - Added constructor and getter for jackson

--- SearchService/src/main/java/com/bush/search/domain/index/service/Operation.java
+++ SearchService/src/main/java/com/bush/search/domain/index/service/Operation.java
@@ -1,12 +1,29 @@
 package com.bush.search.domain.index.service;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Getter
 public enum Operation {
-    CREATE("c"), READ("r"), UPDATE("u"), DELETE("d");
+    CREATE("C"), READ("R"), UPDATE("U"), DELETE("D");
 
     private final String code;
+
+    @JsonValue
+    public String getCode() {
+        return code;
+    }
+
+    @JsonCreator
+    public static Operation fromCode(String code) {
+        for (Operation operation : Operation.values()) {
+            if (operation.code.equals(code)) {
+                return operation;
+            }
+        }
+        throw new IllegalArgumentException("Unknown CRUD operation");
+    }
 }
