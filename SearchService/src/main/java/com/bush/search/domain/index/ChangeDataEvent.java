b16abdaeb0c93d3ebfa1eb28bf3416fa248c1968
Dmitry Bush <bushuev0458@gmail.com>
2026-01-18 16:01:42 +0300

    fix(json):Deleted irrelevant @JsonUnwrapped

--- SearchService/src/main/java/com/bush/search/domain/index/ChangeDataEvent.java
+++ SearchService/src/main/java/com/bush/search/domain/index/ChangeDataEvent.java
@@ -2,7 +2,6 @@ package com.bush.search.domain.index;
 
 import com.bush.search.domain.index.service.Operation;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
 
 import java.time.ZonedDateTime;
 import java.util.UUID;
@@ -11,5 +10,5 @@ public record ChangeDataEvent<T>(@JsonProperty("operation_id") UUID operationId,
                                  @JsonProperty("object_name") String objectName,
                                  @JsonProperty("operation_type") Operation operationType,
                                  @JsonProperty("created_at") ZonedDateTime createdAt,
-                                 @JsonUnwrapped T payload) {
+                                 T payload) {
 }
