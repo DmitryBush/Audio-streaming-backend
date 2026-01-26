5610a54eb9fc5f6f0d5388f6bd2391cd20a66ef1
Dmitry Bush <bushuev0458@gmail.com>
2026-01-26 20:07:15 +0300

    fix:Added eureka url configuration

--- UserService/src/main/java/com/bush/UserServiceApplication.java
+++ UserService/src/main/java/com/bush/UserServiceApplication.java
@@ -3,9 +3,11 @@ package com.bush;
 import com.bush.outbox.annotation.EnableOutboxService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableOutboxService
+@EnableDiscoveryClient
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
