c3e42d7d36742b615541eeb51ff90735606cec48
Dmitry Bush <bushuev0458@gmail.com>
2026-01-28 22:42:46 +0300

    doc(uuid):Created javadoc for UUID adapter

--- UuidAdapter/src/main/java/com/bush/adapter/uuid/UuidTimeEpochGeneratorAdapter.java
+++ UuidAdapter/src/main/java/com/bush/adapter/uuid/UuidTimeEpochGeneratorAdapter.java
@@ -6,6 +6,9 @@ import org.hibernate.id.uuid.UuidValueGenerator;
 
 import java.util.UUID;
 
+/**
+ * Adapter class for Hibernate that implements UUIDv7 generation
+ */
 public class UuidTimeEpochGeneratorAdapter implements UuidValueGenerator {
     @Override
     public UUID generateUuid(SharedSessionContractImplementor sharedSessionContractImplementor) {
