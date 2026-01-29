08fcff7339c854b43a44811b7c9a17caeb26664c
Dmitry Bush <bushuev0458@gmail.com>
2026-01-29 11:24:12 +0300

    docs(artist):Created artist javadoc

--- StreamingService/src/main/java/ohio/rizz/streamingservice/dto/ArtistReadDto.java
+++ StreamingService/src/main/java/ohio/rizz/streamingservice/dto/ArtistReadDto.java
@@ -1,4 +1,9 @@
 package ohio.rizz.streamingservice.dto;
 
+/**
+ * DTO to get information about artist
+ * @param id Unique artist identifier
+ * @param name Name of artist
+ */
 public record ArtistReadDto(Long id, String name) {
 }
