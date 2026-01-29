c796870b00f9194a08539f9a3d247b3a1095e87c
Dmitry Bush <bushuev0458@gmail.com>
2026-01-29 13:21:56 +0300

    Fixed target mapper name

--- StreamingService/src/main/java/ohio/rizz/streamingservice/service/album/mapper/AlbumCreateMapper.java
+++ StreamingService/src/main/java/ohio/rizz/streamingservice/service/album/mapper/AlbumCreateMapper.java
@@ -9,6 +9,6 @@ import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AlbumCreateMapper {
-    @Mapping(target = "coverArtUrl", source = "artworkDto.objectStorageLink")
+    @Mapping(target = "coverArtStorageLink", source = "artworkDto.objectStorageLink")
     Album mapToAlbum(AlbumDto albumDto);
 }
