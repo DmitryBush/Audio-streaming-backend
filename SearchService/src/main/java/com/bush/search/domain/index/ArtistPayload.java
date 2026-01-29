fb86590ab087493d94b8b2e78c51bb8b738cfe5a
Dmitry Bush <bushuev0458@gmail.com>
2026-01-29 15:24:04 +0300

    docs(index):Added javadoc to records

--- SearchService/src/main/java/com/bush/search/domain/index/ArtistPayload.java
+++ SearchService/src/main/java/com/bush/search/domain/index/ArtistPayload.java
@@ -1,5 +1,8 @@
 package com.bush.search.domain.index;
 
+/**
+ * The payload of artist data used in the CDC event
+ */
 public record ArtistPayload(Long id,
                             String name,
                             String biography) {
