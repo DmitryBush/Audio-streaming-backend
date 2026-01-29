ba56f4d1421f2b4d99241540f21b787bc0cfb387
Dmitry Bush <bushuev0458@gmail.com>
2026-01-29 11:27:55 +0300

    docs(genre):Created javadoc for genre

--- StreamingService/src/main/java/ohio/rizz/streamingservice/dto/GenreReadDto.java
+++ StreamingService/src/main/java/ohio/rizz/streamingservice/dto/GenreReadDto.java
@@ -1,4 +1,9 @@
 package ohio.rizz.streamingservice.dto;
 
+/**
+ * DTO to get information about genre
+ * @param id Unique genre identifier
+ * @param name Name of genre
+ */
 public record GenreReadDto(Short id, String name) {
 }
